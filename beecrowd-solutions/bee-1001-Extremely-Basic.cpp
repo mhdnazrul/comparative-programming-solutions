128267c7943347e6d715185d31ff7e6be7ca81f0
Nazrul Islam <mhdnazrul511@gmail.com>
2025-03-03 06:54:00 +0600

    first commit

---
+++ beecrowd-solutions/bee-1001-Extremely-Basic.cpp
@@ -0,0 +1,10 @@
+// bee-1001-Extremely-Basic
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int A, B;
+  cin >> A >> B;                   // value input
+  cout << "X = " << A + B << endl; // sum == A+B
+  return 0;
+}
