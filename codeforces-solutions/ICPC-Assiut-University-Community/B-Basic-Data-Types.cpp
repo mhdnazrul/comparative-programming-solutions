ae0d55acf00e8ff34bd71bc03fc6d136555f9d5b
Nazrul Islam <mhdnazrul511@gmail.com>
2025-03-13 11:22:15 +0600

    added ICPC Assiut University Community solutions

---
+++ codeforces-solutions/ICPC-Assiut-University-Community/B-Basic-Data-Types.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int int_value;
+  long long long_value;
+  char ch;
+  float float_value;
+  double double_value;
+  cin >> int_value >> long_value >> ch >> float_value >> double_value;
+  cout << int_value << endl;
+  cout << long_value << endl;
+  cout << ch << endl;
+  cout << float_value << endl;
+  cout << double_value << endl;
+  return 0;
+}
