612c283220b46a980d0f127e9df3cfe56dcdeb45
Nazrul Islam <mhdnazrul511@gmail.com>
2025-03-13 11:52:24 +0600

    add solutions

---
+++ codeforces-solutions/ICPC-Assiut-University-Community/E-Area-of-a-Circle.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+  double R, Area, pi = 3.141592653;
+  cin >> R;
+  Area = pi * pow(R, 2);
+  cout << fixed << setprecision(9);
+  cout << Area << endl;
+  return 0;
+}
